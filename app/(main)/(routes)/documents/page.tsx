e64975dd0052d9c6175f5e00d469c131440b5c73
Muhtasir Shafkat <msm.shafkat99@gmail.com>
2023-10-18 13:14:48 +0600

    user login and out

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
-
+"use client";
+import Image from "next/image";
+import React from "react";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 const DocumentsPage = () => {
-  return (
-    <div>This is a protected Document page</div>
-  )
-}
+	const { user } = useUser();
+	return (
+		<div className="h-full flex flex-col items-center justify-center space-y-4">
+			<Image
+				src="/empty.png"
+				alt="empty"
+				height="300"
+				width="300"
+				className="dark:hidden"
+			/>
+			<Image
+				src="/empty-dark.png"
+				alt="empty"
+				height="300"
+				width="300"
+				className="hidden dark:block"
+			/>
+			<h2 className="text-lg font-medium ">
+				Welcome to {user?.firstName}&apos;s Botion
+			</h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2"/>
+        Create a note
+      </Button>
+		</div>
+	);
+};
 
-export default DocumentsPage
+export default DocumentsPage;
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,6 +4,7 @@ import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import UserItem from "./user-item";
 const Navigation = () => {
 	const pathname = usePathname();
 	const isMobile = useMediaQuery("(max-width:768px)");
@@ -109,7 +110,7 @@ const Navigation = () => {
 					<ChevronsLeft className="h-6 w-6" />
 				</div>
 				<div>
-					<p>Action Items</p>
+					<UserItem/>
 				</div>
 				<div className="mt-4">
 					<p>Documents</p>
